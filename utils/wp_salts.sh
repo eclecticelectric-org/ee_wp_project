3083e34d7b445606a953d5d88dd360f41bffd75d
rhaft <russ@eclecticelectric.com>
2021-05-09 07:54:55 +0000

    Fixed problem with escape char presence in wp_salts

--- utils/wp_salts.sh
+++ utils/wp_salts.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #
-# Copyright 2019 Eclectic Electric, Inc.
+# Copyright 2019,2021 Eclectic Electric, Inc.
 # https://www.eclecticelectric.com
 #
 #----------------------------
 # generate wordpress salts
 #----------------------------
+
+echo '<?php'
 
 # use chars per wp_salt() in wp-includes/pluggable.php
 ALPHANUM_CHARS="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -13,12 +15,10 @@ SPECIALS_CHARS='!@#$%^&*()'
 EXTRA_SPECIAL_CHARS='-_ []{}<>~`+=,.;:/?|'
 CHARS=${ALPHANUM_CHARS}${SPECIAL_CHARS}${EXTRA_SPECIAL_CHARS}
 
-echo '<?php'
-
-for i in AUTH_KEY SECURE_AUTH_KEY LOGGED_IN_KEY NONCE_KEY AUTH_SALT SECURE_AUTH_SALT LOGGED_IN_SALT NONCE_SALT
+for key in AUTH_KEY SECURE_AUTH_KEY LOGGED_IN_KEY NONCE_KEY AUTH_SALT SECURE_AUTH_SALT LOGGED_IN_SALT NONCE_SALT
 do
 #    echo Processing key: $i
-    SALT=`</dev/urandom tr -dc "$CHARS" | head -c 64  ; echo`
-    KEY=\'$i\',
+    SALT=`</dev/urandom tr -dc "$CHARS" | tr '\\\' '+' | head -c 64  ; echo`
+    KEY=\'$key\',
     printf "define(%-19s '%s');\n" "$KEY" "$SALT"
 done
